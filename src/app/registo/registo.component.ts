2687daee4384e48aa6d4587e339076f715759007
Evora-21805468 <Gofuckyourself98>
2021-01-11 17:24:16 +0000

    criei auth do email e passe

--- src/app/registo/registo.component.ts
+++ src/app/registo/registo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
+import firebase from 'firebase';
+
 
 @Component({
   selector: 'app-registo',
@@ -31,6 +33,20 @@ export class RegistoComponent implements OnInit {
       password: new FormControl('', [Validators.required]),
       confirmarPassword: new FormControl('', [Validators.required]),
     });
+  }
+
+  registoStore() : void {
+    if (this.registoForm.controls['email'].value != null && this.registoForm.controls['password'].value != null) {
+      firebase.auth().createUserWithEmailAndPassword(this.registoForm.controls['email'].value, this.registoForm.controls['password'].value)
+        .then((user) => {
+
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+        });
+    }
   }
 
   onSubmit():void{

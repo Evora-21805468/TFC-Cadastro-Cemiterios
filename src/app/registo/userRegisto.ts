3f7ceeebd1d54d4766a5c38d372da7a3aba5bbf8
Martim <a21805485@alunos.ulht.pt>
2021-01-11 16:29:42 +0000

    Mudei o userRegisto.ts

--- src/app/registo/userRegisto.ts
+++ src/app/registo/userRegisto.ts
@@ -1,16 +1,16 @@
 export interface IUserRegisto{
   email?: string;
   password?: string;
-  primeiroNome?: string;
-  segundoNome?: string;
+  nome?: string;
+  isAdmin?: boolean;
 }
 
 export class UserRegisto implements  IUserRegisto{
   constructor(
     email?: string,
     password?: string,
-    primeiroNome?: string,
-    segundoNome?: string
+    nome?: string,
+    isAdmin?: boolean
   ) {
   }
 }

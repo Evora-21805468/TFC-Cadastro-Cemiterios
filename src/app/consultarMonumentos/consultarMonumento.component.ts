253062b8b0bf2788e7b6a0af1a32741f3a2cf1c1
martim <a21805485@alunos.ulht.ot>
2021-03-09 17:20:09 +0000

    get bd

--- src/app/consultarMonumentos/consultarMonumento.component.ts
+++ src/app/consultarMonumentos/consultarMonumento.component.ts
@@ -8,6 +8,8 @@ import {Subject} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {RequestMethod, RequestOptions} from "@angular/http";
 import {combineAll} from "rxjs/operators";
+import {IUserDB} from "../consultarUsers/userDB";
+import { Monumento} from "./monumento";
 
 
 @Component({
@@ -18,31 +20,61 @@ import {combineAll} from "rxjs/operators";
 export class ConsultarMonumentoComponent implements OnInit {
 
    token: String = "";
+   monumentos: Monumento[] = [];
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private authService: AuthService,
-    public af: AngularFirestore,
-    public angularAuth: AngularFireAuth,
-    private http: HttpClient,
+    constructor(
+      private formBuilder: FormBuilder,
+      private router: Router,
+      private authService: AuthService,
+      public af: AngularFirestore,
+      public angularAuth: AngularFireAuth,
+      private http: HttpClient,
   ) {
 
   }
   private unsubscribe: Subject<void> = new Subject<void>();
 
   async ngOnInit() {
-    let a = await this.ultimatentativa();
+    let a = await this.getToken();
     // @ts-ignore
     this.token = a['access_token'];
     console.log(this.token);
+    for(let i= 1; i < 10;i++){
+      let b = await this.getMonumentos(i);
+      try{
+        // @ts-ignore
+        let id = b['feature']['attributes']['globalid'];
+        // @ts-ignore
+        let numeroMonumento = b['feature']['attributes']['nome_do_monumento'];
+        // @ts-ignore
+        let rua = b['feature']['attributes']['rua'];
+        // @ts-ignore
+        let numeroRua = b['feature']['attributes']['numero_da_rua'];
+        var monumento: Monumento = new Monumento(id,numeroMonumento,rua,numeroRua);
+        console.log(monumento);
+        // @ts-ignore
+        this.monumentos.push(monumento);
+
+      }catch (ex){
+
+      }
+    }
+    console.log(this.monumentos);
   }
 
 
-  async ultimatentativa(){
+  async getToken(){
     let i = ""
     let body = new HttpParams().set('client_id', 'XXScCjKChAwLaplj').set('client_secret', 'd32e344e3b144845a07d75fef9cd9b9e').set('grant_type','client_credentials');
     let response: Promise<Object> = this.http.post('https://www.arcgis.com/sharing/rest/oauth2/token', body).toPromise();
+    return response;
+  }
+
+  async getMonumentos(num: number){
+    let i = ""
+    let body = new HttpParams().set('f', 'json').set('token', this.token.toString());
+    let url = 'https://services-eu1.arcgis.com/kJpwBKPhHXDuJncY/arcgis/rest/services/survey123_1278669bc6f64f089d84969cb31c3aa7/FeatureServer/survey/' + num;
+    let response: Promise<Object> = this.http.post(url, body).toPromise();
     return response;
   }
 
---
+++ src/app/consultarMonumentos/monumento.ts
@@ -0,0 +1,17 @@
+
+export class Monumento {
+   _id: string;
+   _numeroMonumento: string;
+   _rua: string;
+   _numeroRua: string;
+
+  constructor(id: string, numeroMonumento: string, rua: string, numeroRua: string) {
+    this._id = id;
+    this._numeroMonumento = numeroMonumento;
+    this._rua = rua;
+    this._numeroRua = numeroRua;
+  }
+
+
+}
+
